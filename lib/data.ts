2b772608256ded7bd5547574d4f650b81177db88
Bleriot Noguia <nstevebleriot@yahoo.fr>
2023-11-27 15:11:41 +0100

    json data added

---
+++ lib/data.ts
@@ -0,0 +1,130 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import welediImg from "@/public/weledi.png";
+import greatpayImg from "@/public/greatpay.png";
+import digintuCodeImg from "@/public/digintu-code.png";
+import onebillImg from "@/public/onebill.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Full-Stack Developer",
+    company: "ALC-Digital",
+    description:
+      "As a full-stack developer, I worked on web application for managing deliveries and customer orders.",
+    icon: React.createElement(CgWorkAlt),
+    date: "March 2019 - Oct. 2019",
+  },
+  {
+    title: "Full-Stack Developer",
+    company: "ADAA SAS",
+    description:
+      "I worked as a Full-Stack Developer. With my team we build an online learning platform.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Dec. 2019 - Oct. 2020",
+  },
+  {
+    title: "Full-Stack Developer",
+    company: "COMEUP",
+    description:
+      "I worked as a fullstack developer. I discussed with many client",
+    icon: React.createElement(CgWorkAlt),
+    date: "Nov. 2020 - Jul. 2021",
+  },
+  {
+    title: "Full-Stack Developer",
+    company: "Digintu Tech",
+    description:
+      "As a full-stack developer, I participate on building a no-code platform.",
+    icon: React.createElement(FaReact),
+    date: "Aug. 2021 - Nov. 2022",
+  },
+  {
+    title: "Full-Stack Developer",
+    company: "Wat & Co",
+    description:
+      "I worked as a lead front-end developer. I was in charge of the front-end architecture and the development of the web application.",
+    icon: React.createElement(FaReact),
+    date: "Feb. 2023 - Oct. 2023",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Weledi",
+    description:
+      "I worked as a full-stack developer on this website project for 2 months. Visitors can get more information about weledi company.",
+    tags: ["React", "Next.js", "Sanity", "Bootstrap", "TypeScript"],
+    imageUrl: welediImg,
+  },
+  {
+    title: "GreatPay",
+    description:
+      "It is a payment application intended for financial institutions wishing to issue bank cards. I worked on this project as a lead front-end developer.",
+    tags: ["React", "TypeScript", "GitLab", "Chakra UI", "React Query"],
+    imageUrl: greatpayImg,
+  },
+  {
+    title: "Digintu Code",
+    description:
+      "This platform aims to allow freelancers and companies to easily create web applications without having to write a line of code.",
+    tags: ["React", "Typescript", "Ant Design", "GraphQL", "Apollo", "Laravel"],
+    imageUrl: digintuCodeImg,
+  },
+  {
+    title: "OneBill",
+    description:
+      "This application helps companies automatically manage the sending, receiving and payment of their invoices.",
+    tags: ["React", "Typescript", "Ant Design", "Cypress", "Git", "Laravel"],
+    imageUrl: onebillImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Figma",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "Express",
+  "PostgreSQL",
+  "React Query",
+  "Laravel",
+  "Framer Motion",
+] as const;
