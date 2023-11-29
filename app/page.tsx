561b662702987ddf323a29e4cc12e437aa8e079e
Bleriot Noguia <nstevebleriot@yahoo.fr>
2023-11-29 14:07:21 +0100

    Add header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Header from "@/components/header";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <Header />
     </main>
-  )
+  );
 }
