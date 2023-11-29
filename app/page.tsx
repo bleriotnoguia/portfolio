6c62334f7a26bd9d54d1cdc5aade32014b12d266
Bleriot Noguia <nstevebleriot@yahoo.fr>
2023-11-29 16:36:05 +0100

    Add SectionDivider component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Intro />
+      <SectionDivider />
     </main>
   );
 }
