1bb9aede7e37d97017a01ed9b9e2675ded6e437c
Bleriot Noguia <nstevebleriot@yahoo.fr>
2023-11-29 15:41:42 +0100

    Refactor layout and add intro component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Header from "@/components/header";
-import Image from "next/image";
+import Intro from "@/components/intro";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header />
+      <Intro />
     </main>
   );
 }
