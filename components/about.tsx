e8077622cefd96b2f7e98ee73a0a0b40bad7417d
Bleriot Noguia <nstevebleriot@yahoo.fr>
2023-12-04 13:50:23 +0100

    auto select about and projects menu

--- components/about.tsx
+++ components/about.tsx
@@ -3,10 +3,13 @@
 import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function About() {
+  const { ref } = useSectionInView("About", 0.5);
   return (
     <motion.section
+      ref={ref}
       className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
