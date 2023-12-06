5c38550a3900c5a09bfb7f6e0e7afe9ef2b43657
Bleriot Noguia <nstevebleriot@yahoo.fr>
2023-12-06 18:31:37 +0100

    data updated

--- lib/data.ts
+++ lib/data.ts
@@ -46,7 +46,7 @@ export const experiencesData = [
     title: "Fontend Developer",
     company: "Wat & Co",
     description:
-      "I worked as a front-end developer. I was in charge of the front-end architecture and the development of the website.",
+      "I worked as a front-end developer. I was in charge of front-end component development and project management.",
     icon: React.createElement(FaReact),
     date: "Feb. 2023 - Oct. 2023",
   },
@@ -54,7 +54,7 @@ export const experiencesData = [
     title: "Full-Stack Developer",
     company: "Digintu Tech",
     description:
-      "As a full-stack developer, I participate on building a no-code platform.",
+      "As a full-stack developer, I participate on building a no-code platform. We used React.js, Laravel, graphql...",
     icon: React.createElement(FaReact),
     date: "Aug. 2021 - Nov. 2022",
   },
@@ -62,7 +62,7 @@ export const experiencesData = [
     title: "Full-Stack Developer",
     company: "COMEUP",
     description:
-      "I worked as a fullstack developer. I discussed with many client",
+      "On the comeup freelance platform I met the needs of many clients. (website creation and bug fixing)",
     icon: React.createElement(CgWorkAlt),
     date: "Nov. 2020 - Jul. 2021",
   },
@@ -70,7 +70,7 @@ export const experiencesData = [
     title: "Full-Stack Developer",
     company: "ADAA SAS",
     description:
-      "I worked as a Full-Stack Developer. With my team we build an online learning platform.",
+      "I worked as a Full-Stack Developer. With my team we build an online learning platform with React.js and Laravel.",
     icon: React.createElement(CgWorkAlt),
     date: "Dec. 2019 - Oct. 2020",
   },
@@ -88,7 +88,7 @@ export const projectsData = [
   {
     title: "Weledi",
     description:
-      "I worked as a full-stack developer on this website project for 2 months. Visitors can get more information about weledi company.",
+      "I worked as a font-end developer on this website project for 2 months. the website allow visitors to get more information about weledi company.",
     tags: ["React", "Next.js", "Sanity", "Bootstrap", "TypeScript"],
     imageUrl: welediImg,
     link: "https://weledi.dev",
@@ -96,15 +96,23 @@ export const projectsData = [
   {
     title: "Digintu Code",
     description:
-      "This platform aims to allow freelancers and companies to easily create web applications without having to write a line of code.",
-    tags: ["React", "Typescript", "Ant Design", "GraphQL", "Apollo", "Laravel"],
+      "As a fullstack developer, I fully developed the front-end part and I also took care of adding new features to the backend.",
+    tags: [
+      "React",
+      "Typescript",
+      "Ant Design",
+      "Redux",
+      "GraphQL",
+      "Apollo",
+      "Laravel",
+    ],
     imageUrl: digintuCodeImg,
     link: "https://digintu.codes",
   },
   {
     title: "OneBill",
     description:
-      "This application helps companies automatically manage the sending, receiving and payment of their invoices.",
+      "My participation in this project mainly consisted of carrying out end-to-end tests with cypress and fixing bugs in the frontend and backend.",
     tags: ["React", "Typescript", "Ant Design", "Cypress", "Git", "Laravel"],
     imageUrl: onebillImg,
     link: "https://www.icabedo.ch/onebill",
@@ -112,15 +120,15 @@ export const projectsData = [
   {
     title: "Dental Clinic",
     description:
-      "This application helps companies automatically manage the sending, receiving and payment of their invoices.",
-    tags: ["React", "Typescript", "Ant Design", "Cypress", "Git", "Laravel"],
+      "The website was built with WordPress. It allow visitors to get more information about the dental clinic.",
+    tags: ["WordPress", "CSS"],
     imageUrl: clinicImg,
     link: "https://cliniquedentairewado.com",
   },
   {
     title: "GreatPay",
     description:
-      "It is a payment application intended for financial institutions wishing to issue bank cards. I worked on this project as a lead front-end developer.",
+      "At the client's request, I just had to participate in the development of the frontend interfaces, the backend dev having to be carried out by another company.",
     tags: ["React", "TypeScript", "GitLab", "Chakra UI", "React Query"],
     imageUrl: greatpayImg,
     link: "https://greatpay.vercel.app/",
@@ -128,56 +136,55 @@ export const projectsData = [
   {
     title: "Innov and Co",
     description:
-      "This application helps companies automatically manage the sending, receiving and payment of their invoices.",
-    tags: ["React", "Typescript", "Ant Design", "Cypress", "Git", "Laravel"],
+      "The company wanted a modern design that would allow visitors to obtain information about the company quickly.",
+    tags: ["HTML", "CSS", "JavaScript", "jekyll"],
     imageUrl: innovImg,
     link: "https://www.innovandco.net",
   },
   {
     title: "Ga Academie",
     description:
-      "This application helps companies automatically manage the sending, receiving and payment of their invoices.",
-    tags: ["React", "Typescript", "Ant Design", "Cypress", "Git", "Laravel"],
+      "The company wanted a modern design that would allow visitors to obtain information about the company quickly.",
+    tags: ["WordPress", "CSS", "elementor"],
     imageUrl: gaAcademieImg,
     link: "https://gaacademie.com",
   },
   {
     title: "Cointracker",
     description:
-      "This application helps companies automatically manage the sending, receiving and payment of their invoices.",
-    tags: ["React", "Typescript", "Ant Design", "Cypress", "Git", "Laravel"],
+      "Simple clone of Coinmarketcap wallet. Built with React, Redux and Tailwind CSS for learning purposes.",
+    tags: ["React", "Redux", "JavaScript", "Tailwind CSS"],
     imageUrl: cointrackerImg,
     link: "https://cointracker.bleriotnoguia.com/",
   },
   {
     title: "Calculator",
-    description:
-      "This application helps companies automatically manage the sending, receiving and payment of their invoices.",
-    tags: ["React", "Typescript", "Ant Design", "Cypress", "Git", "Laravel"],
+    description: "Simple calculator built with Reactjs for learning purposes.",
+    tags: ["React", "JavaScript", "CSS"],
     imageUrl: calcoImg,
     link: "https://calculator.bleriotnoguia.com/",
   },
   {
     title: "Adaa Learning",
     description:
-      "This application helps companies automatically manage the sending, receiving and payment of their invoices.",
-    tags: ["React", "Typescript", "Ant Design", "Cypress", "Git", "Laravel"],
+      "As a full-stack developer and project manager, I participated in the development of frontend and backend features.",
+    tags: ["React", "Typescript", "Bootstrap", "CSS", "Git", "Laravel"],
     imageUrl: adaaLearningImg,
     link: "https://adaalearning.com/",
   },
   {
     title: "Current portfolio",
     description:
-      "This application helps companies automatically manage the sending, receiving and payment of their invoices.",
-    tags: ["React", "Typescript", "Ant Design", "Cypress", "Git", "Laravel"],
+      "I wanted to have a more modern design for my portfolio. So I built this, also in order to present information about my profile.",
+    tags: ["React", "Typescript", "Next.js", "Tailwind", "Git"],
     imageUrl: portfoliov2Img,
     link: "",
   },
   {
     title: "Portfolio v1",
     description:
-      "This application helps companies automatically manage the sending, receiving and payment of their invoices.",
-    tags: ["React", "Typescript", "Ant Design", "Cypress", "Git", "Laravel"],
+      "This is the first version of my website. I built it in 2018. The goal was just to have a personal website.",
+    tags: ["HTML", "CSS", "JavaScript", "Bootstrap", "Git"],
     imageUrl: porfoliov1Img,
     link: "https://v1.bleriotnoguia.com/",
   },
