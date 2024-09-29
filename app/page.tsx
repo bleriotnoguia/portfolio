e9a1be3727baf59e14c535b0831a53128cce548f
Bleriot Noguia <nstevebleriot@yahoo.fr>
2024-09-29 22:54:54 +0100

    feat: Add Facebook Pixel tracking code to home page
    Add the necessary code to implement Facebook Pixel tracking on the home page. This code allows us to track page views and other events using Facebook's analytics platform.

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
+import Script from "next/script";
 
 export default function Home() {
   return (
@@ -16,6 +17,28 @@ export default function Home() {
       <Skills />
       <Experience />
       <Contact />
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1207669810373228');
+          fbq('track', 'PageView');
+          `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1207669810373228&ev=PageView&noscript=1"
+        />
+      </noscript>
     </main>
   );
 }
